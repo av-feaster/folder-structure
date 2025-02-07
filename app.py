b2a7b8a963e2ad7426f2a42265506596fff0761e
Aman <a4amanver15@gmail.com>
2025-02-07 16:36:01 +0530

    sponsor button

--- app.py
+++ app.py
@@ -117,5 +117,5 @@ def folder_structure_api():
 
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=5100)
